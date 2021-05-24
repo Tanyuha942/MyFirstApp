325fdd19dd3b0a02ba7f7acaa257858b01a7a9f7
Tanya <tanyuha942@gmail.com>
2021-05-25 00:25:00 +0300

    Add method setStr

---
+++ src/FirstApp.java
@@ -0,0 +1,10 @@
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("FirstApp.main");
+        setStr();
+    }
+
+    public static void setStr() {
+        System.out.println("Print my first program!");
+    }
+}
